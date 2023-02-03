d10d82bfe5fcbf1a334304c0f03fe12a66466890
gmh4589 <41452060+gmh4589@users.noreply.github.com>
2023-02-03 12:39:46 +0300

    Add files via upload

--- new_translator.py
+++ new_translator.py
@@ -74,7 +74,7 @@ class MainScreen(Screen):
             p = 0
             for row in range(1, rows):
                 n = int((100/rows) * row)
-                if n != p: print(f'ЗАГРУЗКА {n}%...')
+                if n != p and n % 10 == 0: print(f'ЗАГРУЗКА {n}%...')
                 p = n
                 for col in range(1, cols):
                     cell = self.sheet.cell(row = row, column = col).value
@@ -121,13 +121,21 @@ class MainScreen(Screen):
 
         def getHead(t):
             i = 0
+
+            t = t.replace("'", '')
+
             for i in range(len(t)):
+                if t[i] == '.':
+                    i += 1
+                    break
                 if t[i].isalpha():
-                    if t[i].islower():
-                        print(i)
-                        break
-            print(i)
-            return i - 1
+                    if t[i].islower(): break
+            else:
+                if t[-1] == 'I' or t[-1] == 'A': i = 0
+
+            n = (i - 1)
+            if "(" in t: n -= 1
+            return n
 
         old = self.values[self.sss][0].split(' ')[0]
         h = getHead(old.split(' ')[0])
@@ -140,14 +148,15 @@ class MainScreen(Screen):
             elif step == '-':
                 if self.sss != 0: self.sss -= 1
                 else: Factory.EOFPopup().open()
-            else: self.sss = step
+            else: self.sss = int(step)
 
             o = 1 if self.values[self.sss][1] != '' else 0
 
             ttt = str(self.values[self.sss][o])
 
             hText = ttt.split(' ')[0]
-            head = getHead(hText)
+            if hText[0] == '#': head = getHead(hText)
+            else: head = 0
             self.ids['newTXT'].text = ttt.replace(hText[:head], '')
             self.ids['originalTXT'].text = ttt
 
@@ -167,7 +176,7 @@ class MainScreen(Screen):
         self.ids['newTXT'].text = self.ids['originalTXT'].text
 
     def findBTN(self):
-        global count
+
         text4find = str(self.ids['text4find'].text)
         text4replace = str(self.ids['text4replace'].text)
         count = 0
@@ -190,8 +199,8 @@ class MainScreen(Screen):
 
     def gotoBTN(self):
         try:
-            goto = self.ids['gotoInput'].text
-            if int(goto) < len(self.values) + 1: self.nextString(int(goto) - 1)
+            goto = int(self.ids['gotoInput'].text)
+            if goto < len(self.values) + 1: self.nextString(goto - 1)
             else: mb.showinfo(title = 'Сообщение', message = f'Вы ввели {goto}, а в файле только {len(self.values)} строк!')
         except (TypeError, ValueError): mb.showinfo(title = 'Сообщение', message = f'Введите число от 1 до {len(self.values)}!')
 
@@ -202,9 +211,10 @@ class MainScreen(Screen):
         elif setting['Main']['translator'] == 'Bing': site = 'https://www.bing.com/translator/?text='
         elif setting['Main']['translator'] == 'DeepL': site = 'https://www.deepl.com/translator#en/ru/'
         elif setting['Main']['translator'] == 'Promt': site = 'https://www.translate.ru/%D0%BF%D0%B5%D1%80%D0%B5%D0%B2%D0%BE%D0%B4/%D0%B0%D0%BD%D0%B3%D0%BB%D0%B8%D0%B9%D1%81%D0%BA%D0%B8%D0%B9-%D1%80%D1%83%D1%81%D1%81%D0%BA%D0%B8%D0%B9?text='
+        else: site = ''
 
         t = self.ids['newTXT'].text.replace('#', '')
-        if t != '': webbrowser.open(site + t)
+        if t != '' and site != '': webbrowser.open(site + t)
 
 class SettingScreen(Screen):
 
